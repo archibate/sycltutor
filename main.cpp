374b9b3b53981a0cea3a6485b34903a8b96fe715
archibate <1931127624@qq.com>
2023-08-26 15:57:35 +0800

    fin

--- main.cpp
+++ main.cpp
@@ -4,67 +4,101 @@
 using namespace sycl;
 
 template <class HistT, class ValT>
-void sycl_histogram(HistT *hist_p, size_t hist_n, ValT const *val_p, size_t val_n) {
+void sycl_histogram(HistT *hist_p, size_t hist_n, ValT const *val_p, size_t val_n, int times) {
     constexpr size_t k_block_size = 256;
     constexpr size_t k_num_bins = 256;
+    constexpr size_t k_group_size = 128;
     static queue q{gpu_selector_v};
     buffer hist_buf{hist_p, range<1>{hist_n}};
     buffer val_buf{val_p, range<1>{val_n}};
-    q.submit([&] (handler &cgh) {
-        accessor hist_axr{hist_buf, cgh, read_write};
-        accessor val_axr{val_buf, cgh, read_only};
-        cgh.parallel_for(nd_range<1>{val_n / k_block_size, 64}, [=] (nd_item<1> it) {
-            auto wg = it.get_group();
-            auto wg_id = wg.get_group_id()[0];
-            auto wg_size = wg.get_local_range()[0];
-            auto sg = it.get_sub_group();
-            auto sg_id = sg.get_group_id()[0];
-            auto sg_size = sg.get_local_range()[0];
-            HistT hist_priv[k_num_bins / 16];
-            for (size_t j = 0; j < k_num_bins / 16; j++) {
-                hist_priv[j] = HistT{};
-            }
-            for (size_t j = 0; j < k_block_size; j++) {
-                ValT v0 = val_axr[wg_id * wg_size * k_block_size
-                    + sg_id * sg_size * k_block_size
-                    + sg_size * j];
-                size_t v = v0 % k_num_bins;
-                #pragma unroll
-                for (size_t i1 = 0; i1 < sg_size; i1++) {
-                    auto c = group_broadcast(sg, v, i1);
-                    if (sg.get_local_id()[0] == (c & 0xf)) {
-                        hist_priv[c >> 4] += HistT{1};
+    for (int i = 0; i < times; i++) {
+        q.submit([&] (handler &cgh) {
+            accessor hist_axr{hist_buf, cgh, read_write};
+            accessor val_axr{val_buf, cgh, read_only};
+            local_accessor<HistT> hist_loc{k_num_bins, cgh};
+            cgh.parallel_for(nd_range<1>{((val_n + k_block_size - 1) / k_block_size + k_group_size - 1) / k_group_size * k_group_size, k_group_size}, [=] (nd_item<1> it) {
+#if 1
+                auto i = it.get_global_id()[0];
+                auto li = it.get_local_id()[0];
+                hist_loc[li * 2] = 0;
+                hist_loc[li * 2 + 1] = 0;
+                it.barrier(access::fence_space::local_space);
+                for (size_t j = 0; j < k_block_size; j++) {
+                    if (i * k_block_size + j < val_axr.size()) {
+                        auto v = val_axr[i * k_block_size + j] % k_num_bins;
+                        atomic_ref<HistT, memory_order_relaxed, memory_scope_device, access::address_space::local_space>{hist_loc[v]}.fetch_add(HistT{1});
                     }
                 }
-            }
-            for (size_t j = 0; j < k_num_bins / 16; j++) {
-                sycl::atomic_ref<HistT, memory_order_relaxed, memory_scope_device>{hist_axr[16 * j + sg.get_local_id()[0]]}.fetch_add(hist_priv[j]);
-            }
-        });
-    }).wait_and_throw();
+                it.barrier(access::fence_space::local_space);
+                atomic_ref<HistT, memory_order_relaxed, memory_scope_device, access::address_space::global_space>{hist_axr[li * 2]}.fetch_add(hist_loc[li * 2]);
+                atomic_ref<HistT, memory_order_relaxed, memory_scope_device, access::address_space::global_space>{hist_axr[li * 2 + 1]}.fetch_add(hist_loc[li * 2 + 1]);
+#else
+                size_t group = it.get_group()[0];
+                size_t gSize = it.get_local_range()[0];
+                auto sg = it.get_sub_group();
+                size_t sgSize = sg.get_local_range()[0];
+                size_t sgGroup = sg.get_group_id()[0];
+
+                size_t factor = k_num_bins / gSize;
+                size_t local_id = it.get_local_id()[0];
+                if (factor <= 1 && local_id < k_num_bins) {
+                    atomic_ref<HistT, memory_order_relaxed, memory_scope_device, access::address_space::local_space>{hist_loc[local_id]}.store(HistT{0});
+                } else {
+                    for (size_t k = 0; k < factor; k++) {
+                        atomic_ref<HistT, memory_order_relaxed, memory_scope_device, access::address_space::local_space>{hist_loc[gSize * k + local_id]}.store(HistT{0});
+                    }
+                }
+                it.barrier(access::fence_space::local_space);
+                auto vbase = (group * gSize + sgGroup * sgSize) * k_block_size;
+                for (size_t k = 0; k < k_block_size; k++) {
+                    auto vid = vbase + sgSize * k;
+                    if (vid < val_axr.size()) {
+                        auto v = val_axr[vid] % k_num_bins;
+                        atomic_ref<HistT, memory_order_relaxed, memory_scope_device, access::address_space::local_space>{hist_loc[v]}.fetch_add(HistT{1});
+                    }
+                }
+                it.barrier(access::fence_space::local_space);
+                if (factor <= 1 && local_id < k_num_bins) {
+                    auto h = atomic_ref<HistT, memory_order_relaxed, memory_scope_device, access::address_space::local_space>{hist_loc[local_id]}.load();
+                    atomic_ref<HistT, memory_order_relaxed, memory_scope_device, access::address_space::global_space>{hist_axr[local_id]}.fetch_add(h);
+                } else {
+                    for (size_t k = 0; k < factor; k++) {
+                        auto v = val_axr[
+                            (group * gSize + sgGroup * sgSize) * k_block_size
+                            + sgSize * k] % k_num_bins;
+                        auto h = atomic_ref<HistT, memory_order_relaxed, memory_scope_device, access::address_space::local_space>{hist_loc[gSize * k + local_id]}.load();
+                        atomic_ref<HistT, memory_order_relaxed, memory_scope_device, access::address_space::global_space>{hist_axr[gSize * k + local_id]}.fetch_add(h);
+                    }
+                }
+#endif
+            });
+        }).wait_and_throw();
+    }
 }
 
 template <class HistT, class ValT>
-void cpu_histogram(HistT *hist_p, size_t hist_n, ValT const *val_p, size_t val_n) {
+void cpu_histogram(HistT *hist_p, size_t hist_n, ValT const *val_p, size_t val_n, int times) {
     memset(hist_p, 0, hist_n * sizeof(HistT));
-    for (size_t i = 0; i < val_n; i++) {
-        ++hist_p[val_p[i]];
+    for (int i = 0; i < times; i++) {
+        for (size_t i = 0; i < val_n; i++) {
+            ++hist_p[val_p[i]];
+        }
     }
 }
 
 namespace _makeAutoBench_details {
 
 template <class T>
-static std::enable_if_t<std::is_integral_v<T>> do_randomize(std::vector<T> &arr, T min, T max) {
-    static thread_local std::mt19937 rng;
+static std::enable_if_t<std::is_integral_v<T>> do_randomize(std::vector<T> &arr, T min, T max, uint32_t seed) {
+    std::mt19937 rng{seed};
     for (auto &a: arr) {
         a = std::uniform_int_distribution<T>{min, max}(rng);
     }
 }
 
 template <class T>
-static std::enable_if_t<std::is_floating_point_v<T>> do_randomize(std::vector<T> &arr, T min, T max) {
-    static thread_local std::mt19937 rng;
+static std::enable_if_t<std::is_floating_point_v<T>> do_randomize(std::vector<T> &arr, T min, T max, uint32_t seed) {
+    std::mt19937 rng{seed};
     for (auto &a: arr) {
         a = std::uniform_real_distribution<T>{min, max}(rng);
     }
@@ -80,7 +114,7 @@ struct AutoBenchArg {
         return inner;
     }
 
-    void init(T v) {
+    void init(T v, size_t) {
         inner = v;
     }
 
@@ -99,7 +133,7 @@ struct AutoBenchArg<T *> {
         return inner.data();
     }
 
-    void init(size_t n) {
+    void init(size_t n, size_t) {
         inner.clear();
         inner.resize(n);
     }
@@ -114,6 +148,16 @@ struct AutoBenchArg<T *> {
             if (inner[i] != that.inner[i]) {
                 std::cerr << "in function #" << funcid << ", argument #" << index << ": ";
                 std::cerr << "element " << i << " mismatched, got " << inner[i] << ", expect " << that.inner[i] << "\n";
+                std::cerr << "[ ";
+                for (size_t j = i > 5 ? i - 5 : 0; j < std::min(inner.size(), i + 5); j++) {
+                    std::cerr << inner[j] << ' ';
+                }
+                std::cerr << "]\n";
+                std::cerr << "[ ";
+                for (size_t j = i > 5 ? i - 5 : 0; j < std::min(inner.size(), i + 5); j++) {
+                    std::cerr << that.inner[j] << ' ';
+                }
+                std::cerr << "]\n";
                 return false;
             }
         }
@@ -131,10 +175,10 @@ struct AutoBenchArg<T const *> {
         return inner.data();
     }
 
-    void init(std::tuple<size_t, T, T> arg) {
+    void init(std::tuple<size_t, T, T> arg, size_t index) {
         auto [n, min, max] = arg;
         inner.resize(n);
-        do_randomize(inner, min, max);
+        do_randomize(inner, min, max, (uint32_t)index);
     }
 
     bool test(AutoBenchArg const &, size_t, size_t) {
@@ -155,18 +199,15 @@ struct AutoBench<N, void(Ts...)> {
     template <size_t ...Is>
     void _impl_add_arguments(std::index_sequence<Is...>,
                          std::conditional_t<false, Ts, typename AutoBenchArg<Ts>::argument_type> ...argvals) const {
-        ((void)std::get<Is>(args).init(std::move(argvals)), ...);
+        ((void)std::get<Is>(args).init(std::move(argvals), Is), ...);
     }
 
     template <size_t ...Is>
-    bool _impl_test(std::index_sequence<Is...>) const {
+    bool _impl_test(std::index_sequence<Is...>, size_t i) const {
         std::tuple<AutoBenchArg<Ts>...> stdargs = args;
         fps[0](std::get<Is>(stdargs).get()...);
-        for (size_t i = 1; i < N; i++)
-            if (((void)fps[i](std::get<Is>(args).get()...),
-                !(std::get<Is>(args).test(std::get<Is>(stdargs), i, Is) && ...))
-            ) return false;
-        return true;
+        fps[i](std::get<Is>(args).get()...);
+        return (std::get<Is>(args).test(std::get<Is>(stdargs), i, Is) && ...);
     }
 
     template <size_t ...Is>
@@ -202,10 +243,12 @@ int doAutoBench(std::string const &title,
     ...argvals) {
     _makeAutoBench_details::AutoBench<N, void(Ts...)> ab{std::move(fps)};
     ab._impl_add_arguments(std::make_index_sequence<sizeof...(Ts)>{}, argvals...);
-    if (!ab._impl_test(std::make_index_sequence<sizeof...(Ts)>{}))
-        std::cerr << "test failed\n";
     for (size_t i = 0; i < N; i++) {
         auto bmfunc = [=] (::benchmark::State &s) {
+            bool testok = i == 0 || (
+                ab._impl_add_arguments(std::make_index_sequence<sizeof...(Ts)>{}, argvals...),
+                ab._impl_test(std::make_index_sequence<sizeof...(Ts)>{}, i));
+            if (!testok) s.SkipWithError("test failed");
             ab._impl_add_arguments(std::make_index_sequence<sizeof...(Ts)>{}, argvals...);
             ab._impl_run(s, i, std::make_index_sequence<sizeof...(Ts)>{});
         };
@@ -215,10 +258,10 @@ int doAutoBench(std::string const &title,
     return 0;
 }
 
-constexpr size_t n = 65536000;
+constexpr size_t n = 65536 * 64;
 constexpr size_t maxval = 256;
 
 static int bench_histogram_uint32_t = doAutoBench("histogram", std::array{
     cpu_histogram<uint32_t, uint32_t>,
     sycl_histogram<uint32_t, uint32_t>,
-}, maxval, maxval, {n, 0, maxval}, n);
+}, maxval, maxval, {n, 0, maxval - 1}, n, 100);
