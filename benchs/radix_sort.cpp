6e603bb1e91964aeec892366807ff54484dbe2c9
archibate <1931127624@qq.com>
2023-08-28 21:14:38 +0800

    benchs

--- benchs/radix_sort.cpp
+++ benchs/radix_sort.cpp
@@ -5,25 +5,6 @@
 #include <execution>
 #include "utils/ticktock.h"
 
-/* template <class T> */
-/* using Buffer = sycl::buffer<T>; */
-/* using Vec3f = sycl::vec<float, 3>; */
-/* using Vec3u = sycl::vec<unsigned, 3>; */
-/*  */
-/* struct Mesh { */
-/*     Buffer<Vec3f> vertices; */
-/*     Buffer<Vec3u> indices; */
-/* }; */
-/*  */
-/* inline void compute_mesh_normal(sycl::queue &q, sycl::buffer<sycl::vec<float, 3>> &vertices, sycl::buffer<sycl::vec<unsigned, 3>> &indices) { */
-/*     if (vertices.size() <= 1) return; */
-/*     q.submit([&] (sycl::handler &cgh) { */
-/*         sycl::accessor vert{vertices, cgh, sycl::read_only}; */
-/*         cgh.parallel_for(sycl::nd_range<1>{vert.size(), 256}, [=] (sycl::nd_item<1> it) { */
-/*         }); */
-/*     }); */
-/* } */
-
 int main() {
     constexpr size_t n = 4 * 256 * 256 * 256;
     {
